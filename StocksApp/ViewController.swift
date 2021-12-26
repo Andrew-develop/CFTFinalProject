d20defa5355aa8c773703ca147ed202f2c06982a
Andrew Frolov <adm.kolchak.221001@gmail.com>
2021-12-26 12:16:28 +0700

    Initial Commit

---
+++ StocksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StocksApp
+//
+//  Created by Sergio Ramos on 26.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
